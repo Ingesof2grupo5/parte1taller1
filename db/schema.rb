77c923c39848e9377ccccd1e6822c74caa21e927
Bryan Antonio Angarita Rodriguez <baangaritar@unal.edu.co>
2017-09-02 15:25:32 +0000

    Car model with scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170902140859) do
+ActiveRecord::Schema.define(version: 20170902151853) do
+
+  create_table "cars", force: :cascade do |t|
+    t.string "brand"
+    t.string "model"
+    t.string "plates"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "customers", force: :cascade do |t|
     t.string "name"
